7937ee7301813cde007b6c78b0e8a7fc598c213d
Jan Kohút <kohut.honza@seznam.cz>
2016-10-11 19:48:21 +0200

    Delete imps.

--- basic.fs
+++
@@ -1,9 +0,0 @@
-#version 150
-
-out vec4 outColor;
-in vec3 inColor;
-
- 
-void main(void) {    
-	outColor = vec4(inColor, 1.0f);
-}
--- camera.cpp
+++
@@ -1,7 +0,0 @@
-#include "camera.h"
-
-Camera::Camera(glm::vec3 &pos, float fov, float aspect, float zNear, float zFar) : up(glm::vec3(0.0, 1.0, 0.0)){
-	 perspective = glm::perspective(fov, aspect, zNear, zFar);
-	 position = pos;
-	 forward = glm::vec3(0.0f, 0.0f, -1.0f);
-}
